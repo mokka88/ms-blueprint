931cd06c807856d0306419215631331bdfd8aa3f
Gábor Motkó <mokka88@gmail.com>
2020-07-08 23:22:12 +0200

    initial spring boot project

---
+++ src/main/java/org/mokka88/msblueprint/MsBlueprintApplication.java
@@ -0,0 +1,13 @@
+package org.mokka88.msblueprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBlueprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBlueprintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mokka88/msblueprint/MsBlueprintApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mokka88.msblueprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBlueprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
